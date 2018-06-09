0a3ab71c190239d2c4065904623e6eea3e8109c0
Hollowsoul3 <deenis033@gmail.com>
2018-06-09 12:56:15 +0300

    no message

--- untitled1/src/Lesson72.java
+++ untitled1/src/Lesson72.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Lesson72 {
     public static void main(String[] args) {
-        int a;
+
         try (Scanner sc = new Scanner(System.in)) {
 
 
@@ -14,11 +14,19 @@ public class Lesson72 {
             }
              */
 
-
+            /*
             do {
                 a = sc.nextInt();
             } while (a != 0);
             System.out.println("Goodbye");
+            */
+            int a, b, res=1;
+            a = sc.nextInt();
+            b = sc.nextInt();
+            for(;b>0;b--){
+                res=res*a;
+            }
+            System.out.println(res);
         }
     }
 }
